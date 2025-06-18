aff920c42b7a0fc7f15a1527cd3c9e6fa59a4adf
thehersh39 <sean@hockeystickventuresllc.com>
2025-06-18 15:37:24 -0500

    Create apollo-proxy.js

---
+++ netlify/functions/apollo-proxy.js
@@ -0,0 +1,56 @@
+exports.handler = async (event, context) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers, body: '' };
+  }
+
+  if (event.httpMethod !== 'POST') {
+    return { 
+      statusCode: 405, 
+      headers, 
+      body: JSON.stringify({ error: 'Method not allowed' }) 
+    };
+  }
+
+  try {
+    const body = JSON.parse(event.body);
+    
+    // Get API key from environment variable instead of hardcoding it
+    const apiKey = process.env.APOLLO_API_KEY;
+    
+    const response = await fetch('https://api.apollo.io/v1/mixed_people/search', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Api-Key': apiKey
+      },
+      body: JSON.stringify(body)
+    });
+
+    const data = await response.json();
+    
+    return {
+      statusCode: 200,
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
